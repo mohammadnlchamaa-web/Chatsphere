586a24ab4d124abb16316cd255c0b761d6f7029a
mohammadnlchamaa-web <mohammad.nl.chamaa@gmail.com>
2026-06-22 18:29:18 +0300

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://chatsphere-backend-6z66.onrender.com");
 
 export default function App() {
   const [user, setUser] = useState(null);
